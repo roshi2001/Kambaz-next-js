768ed67d3c599a1146daccfa1be620e6298c3a05
roshi2001 <roshi2001@users.noreply.github.com>
2025-09-15 13:37:07 -0400

    Updated page with GitHub link

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 export default function Lab1() {
   return (
     <div id="wd-lab1">
@@ -116,21 +116,24 @@ export default function Lab1() {
           </tfoot>
         </table>
       </div>
-
+      
+      
       <div id="wd-images">
         <h4>Image tag</h4>
         Loading an image from the internet: <br />
-        <img
+        <Image
           id="wd-starship"
           width="400px"
           src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
           alt="Starship"
+          width={400}
+          height={225}
         />
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" alt="Tesla Bot" />
-      </div>
+        <Image id="wd-teslabot" src="/images/teslabot.jpg"  width={200} height="200px" alt="Tesla Bot" />
+        </div>
 
       <div id="wd-forms">
         <h4>Form Elements</h4>
@@ -185,7 +188,7 @@ export default function Lab1() {
           onClick={() => {
           const input = document.createElement("input");
           input.type = "file";
-          input.onchange = (e: any) => {
+          input.onchange = (e: string) => {
           const file = e.target.files[0];
           if (file) {
           alert(`Selected file: ${file.name}`);
